7a11431a405bfa5f9348c88dc9f47bae83b968be
Juan Medina <juandavidds08@gmail.com>
2021-10-02 11:40:50 -0500

    Implementacion Base Datos

---
+++ MascotaFeliz.App.Dominio/Entidades/Hospital.cs
@@ -0,0 +1,10 @@
+using System;
+namespace MascotaFeliz.AppDominio
+{
+    public class Hospital
+    {     
+    public string Id_Hospital {get;set;}
+    public string Nombre  {get;set;}
+    
+    }
+}
---
+++ MascotaFeliz.App.Dominio/Entidades/Veterinario.cs
@@ -0,0 +1,11 @@
+using System;
+namespace MascotaFeliz.AppDominio
+{
+    public class Veterinario
+    {     
+    public string Raza {get;set;}
+    public string Sonido  {get;set;}
+    public string Cliente {get;set;}
+    public string Hospital {get;set;}
+    }
+}
